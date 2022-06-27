7e01653d2a15fdc305f0cd6089cf23153c8933f4
Bikash <bikashrai7924@gmail.com>
2022-06-27 10:24:59 +0545

    initiated

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDom from "react-dom";
+import "./mystyle.css";
+
+function App() {
+  return <h3>Hello this is Bikash Rai</h3>;
+}
+ReactDom.render(<App />, document.getElementById("root"));
